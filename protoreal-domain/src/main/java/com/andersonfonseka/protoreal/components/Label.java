2002d4e8a8bfebfcd2f8bb31ce62195f3938ef61
anderson fonseca <anderson.fonseka@gmail.com>
2020-05-05 18:30:23 -0300

    ''

--- protoreal-domain/src/main/java/com/andersonfonseka/protoreal/components/Label.java
+++ protoreal-domain/src/main/java/com/andersonfonseka/protoreal/components/Label.java
@@ -4,12 +4,22 @@ import com.andersonfonseka.protoreal.components.render.LabelRenderer;
 
 public class Label extends Input {
 	
+	private String type = "";
+	
 	public Label() {
 		this("Text");
 	}
 	
 	public Label(String label) {
 		super(label);
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 	
 	@Override
