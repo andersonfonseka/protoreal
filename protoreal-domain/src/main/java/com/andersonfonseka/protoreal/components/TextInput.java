4a5d60e6254ed7a0f99e6c8579f6c3ac353085a8
anderson fonseca <anderson.fonseka@gmail.com>
2020-05-18 19:09:55 -0300

    ''

--- protoreal-domain/src/main/java/com/andersonfonseka/protoreal/components/TextInput.java
+++ protoreal-domain/src/main/java/com/andersonfonseka/protoreal/components/TextInput.java
@@ -1,10 +1,16 @@
 package com.andersonfonseka.protoreal.components;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.andersonfonseka.protoreal.components.common.SelectItem;
 import com.andersonfonseka.protoreal.components.render.TextInputRenderer;
 
 public class TextInput extends Input {
 
 	private String type = "text";
+	
+	private List<SelectItem> listTypes = new ArrayList<SelectItem>();
 	
 	public TextInput() {
 		this("Rotulo");
@@ -20,6 +26,30 @@ public class TextInput extends Input {
 
 	public void setType(String type) {
 		this.type = type;
+	}
+	
+	public List<SelectItem> getListTypes() {
+		
+		listTypes.clear();
+		
+		listTypes.add(new SelectItem("text", "Texto"));
+		listTypes.add(new SelectItem("date", "Data"));
+		listTypes.add(new SelectItem("number", "Numero"));
+		listTypes.add(new SelectItem("password", "Senha"));
+		listTypes.add(new SelectItem("email", "E-mail"));
+		listTypes.add(new SelectItem("file", "Arquivo"));
+		
+		for (SelectItem selectItem : listTypes) {
+			if (selectItem.getValue().equals(this.type)) {
+				selectItem.setSelected("selected");
+			}
+		}
+		
+		return listTypes;
+	}
+
+	public void setListTypes(List<SelectItem> listTypes) {
+		this.listTypes = listTypes;
 	}
 	
 	@Override
