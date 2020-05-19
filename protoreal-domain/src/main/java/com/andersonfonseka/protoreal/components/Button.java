03ebb6a2c8bbfcb683d9160e9fbc4bcad26e0b17
anderson fonseca <anderson.fonseka@gmail.com>
2020-05-19 11:27:50 -0300

    ''

--- protoreal-domain/src/main/java/com/andersonfonseka/protoreal/components/Button.java
+++ protoreal-domain/src/main/java/com/andersonfonseka/protoreal/components/Button.java
@@ -8,25 +8,22 @@ import com.andersonfonseka.protoreal.components.render.ButtonRenderer;
 
 public class Button extends Input {
 	
-	private String cssClass;
-	
-	public static final String PRIMARY = "btn btn-primary";
-	public static final String SECONDARY = "btn btn-secondary";
-	public static final String SUCCESS = "btn btn-success";
-	public static final String DANGER = "btn btn-danger";	
-	public static final String WARNING = "btn btn-warning";
-	public static final String INFO = "btn btn-info";
-	public static final String LIGHT = "btn btn-light";
-	public static final String DARK = "btn btn-dark";
-	public static final String LINK = "btn btn-link";
-	
-	private String openType = "default";
 	
 	private List<SelectItem> selectPages = new ArrayList<SelectItem>();
 	
 	private List<Component> pages = new ArrayList<Component>();
+
+	private String cssClass = "btn btn-primary";
 	
 	private List<SelectItem> cssStyles = new ArrayList<SelectItem>(); 
+	
+	private String openType = "default";
+	
+	private List<SelectItem> navOpenType = new ArrayList<SelectItem>(); 
+	
+	private String alignment = "float-left";
+	
+	private List<SelectItem> alignmentOptions = new ArrayList<SelectItem>(); 
 	
 	private Page page;
 	
@@ -34,7 +31,6 @@ public class Button extends Input {
 	
 	public Button() {
 		super("Button");
-		this.cssClass = PRIMARY;
 	}
 	
 	public Button(String label, String cssClass) {
@@ -121,6 +117,57 @@ public class Button extends Input {
 
 	public void setCssStyles(List<SelectItem> cssStyles) {
 		this.cssStyles = cssStyles;
+	}
+	
+	
+
+	public List<SelectItem> getNavOpenType() {
+		
+		navOpenType.clear();
+		
+		navOpenType.add(new SelectItem("default", "Padrao"));
+		navOpenType.add(new SelectItem("modal", "Modal"));
+		
+		
+		for (SelectItem selectItem : navOpenType) {
+			if (selectItem.getValue().equals(openType)) {
+				selectItem.setSelected("checked");
+			}
+		}
+		
+		return navOpenType;
+	}
+
+	public void setNavOpenType(List<SelectItem> navOpenType) {
+		this.navOpenType = navOpenType;
+	}
+	
+	public String getAlignment() {
+		return alignment;
+	}
+
+	public void setAlignment(String alignment) {
+		this.alignment = alignment;
+	}
+
+	public List<SelectItem> getAlignmentOptions() {
+		
+		alignmentOptions.clear();
+		
+		alignmentOptions.add(new SelectItem("float-left", "Esquerda"));
+		alignmentOptions.add(new SelectItem("float-right", "Direita"));
+		
+		for (SelectItem selectItem : alignmentOptions) {
+			if (selectItem.getValue().equals(alignment)) {
+				selectItem.setSelected("checked");
+			}
+		}
+		
+		return alignmentOptions;
+	}
+
+	public void setAlignmentOptions(List<SelectItem> alignmentOptions) {
+		this.alignmentOptions = alignmentOptions;
 	}
 
 	@Override
